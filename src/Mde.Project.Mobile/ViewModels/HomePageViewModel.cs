559cb07ad2e7ff6bbdc92a5913f21a00d4cc8dd8
Remi Decorte <remi.decorte@student.howest.be>
2025-07-25 00:02:24 +0200

    Add HomepageViewmodel with observablecollection of events  (zorgt voor makkelijk event binden aan collectionview)

---
+++ src/Mde.Project.Mobile/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Mde.Project.Mobile.Models;
+using Mde.Project.Mobile.Services;
+
+namespace Mde.Project.Mobile.ViewModels
+{
+    public class HomePageViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<EventModel> Events { get; private set; } = new();
+        private readonly EventService _eventService = new();
+
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set
+            {
+                _isLoading = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public async Task LoadEventsAsync()
+        {
+            IsLoading = true;
+            var events = await _eventService.GetUpcomingEventsAsync();
+            Events.Clear();
+
+            foreach (var ev in events)
+            {
+                Events.Add(ev);
+            }
+
+            IsLoading = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
