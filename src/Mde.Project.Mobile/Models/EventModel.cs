ba0801662ee999266f4c34ccb25c3ee2a7e5fff4
Remi Decorte <remi.decorte@student.howest.be>
2025-07-24 22:17:42 +0200

    add swagger

---
+++ src/Mde.Project.Mobile/Models/EventModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mde.Project.Mobile.Models
+{
+    public class EventModel
+    {
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
