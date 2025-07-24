6483a488ea50a6c075f7ba0d93be1671be0791a4
Remi Decorte <remi.decorte@student.howest.be>
2025-07-24 21:18:54 +0200

    add Web Api

---
+++ src/Mde.Project.WebApi/Entities/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace Mde.Project.WebApi.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
