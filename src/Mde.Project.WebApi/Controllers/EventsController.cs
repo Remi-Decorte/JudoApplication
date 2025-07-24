e7cee65ec0b1ed85f95b0db49788808a92765ee8
Remi Decorte <remi.decorte@student.howest.be>
2025-07-24 21:37:27 +0200

    add eventscontroller, Dto  and endpoint

---
+++ src/Mde.Project.WebApi/Controllers/EventsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mde.Project.WebApi.Data;
+using Mde.Project.WebApi.DTOs;
+
+namespace Mde.Project.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventsController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public EventsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: /api/events/upcoming
+    [HttpGet("upcoming")]
+    public ActionResult<IEnumerable<EventDto>> GetUpcomingEvents()
+    {
+        var today = DateTime.Today;
+
+        var events = _context.Events
+            .Where(e => e.Date >= today)
+            .OrderBy(e => e.Date)
+            .Take(3)
+            .Select(e => new EventDto
+            {
+                Title = e.Title,
+                Location = e.Location,
+                Date = e.Date
+            })
+            .ToList();
+
+        return Ok(events);
+    }
+}
---
+++ src/Mde.Project.WebApi/DTOs/EventDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mde.Project.WebApi.DTOs
+{
+    public class EventDto
+    {
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
