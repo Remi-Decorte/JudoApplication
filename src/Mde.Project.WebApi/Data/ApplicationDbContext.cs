b310f9cf0594e55b2d77353c8dcb617a992493e0
Remi Decorte <remi.decorte@student.howest.be>
2025-07-25 14:41:41 +0200

    add seeding

--- src/Mde.Project.WebApi/Data/ApplicationDbContext.cs
+++ src/Mde.Project.WebApi/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Mde.Project.WebApi.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mde.Project.WebApi.Data
@@ -11,5 +12,7 @@ namespace Mde.Project.WebApi.Data
         }
 
         public DbSet<Event> Events { get; set; }
+        public DbSet<Judoka> Judokas { get; set}
+        
     }
 }
