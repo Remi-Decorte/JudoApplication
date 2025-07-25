845254ca16fc199dd7575f94fbb05471c2c6191e
Remi Decorte <remi.decorte@student.howest.be>
2025-07-25 14:51:06 +0200

    add judokaservice

---
+++ src/Mde.Project.Mobile/Services/JudokaService.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Json;
+using Mde.Project.Mobile.Models;
+
+namespace Mde.Project.Mobile.Services;
+
+public class JudokaService
+{
+    private readonly HttpClient _httpClient;
+
+    public JudokaService()
+    {
+        _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:62160/") 
+        };
+    }
+
+    public async Task<List<JudokaModel>> GetJudokasByCategoryAsync(string category)
+    {
+        try
+        {
+            var response = await _httpClient.GetFromJsonAsync<List<JudokaModel>>($"api/judokas/by-category/{category}");
+            return response ?? new List<JudokaModel>();
+        }
+        catch
+        {
+            return new List<JudokaModel>(); 
+        }
+    }
+}
