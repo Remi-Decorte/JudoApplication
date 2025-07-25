ac9d0cbaf12c15620c50882b07af431daf654377
Remi Decorte <remi.decorte@student.howest.be>
2025-07-25 14:45:10 +0200

    small fix

--- src/Mde.Project.WebApi/Data/ApplicationDbContext.cs
+++ src/Mde.Project.WebApi/Data/ApplicationDbContext.cs
@@ -12,7 +12,8 @@ namespace Mde.Project.WebApi.Data
         }
 
         public DbSet<Event> Events { get; set; }
-        public DbSet<Judoka> Judokas { get; set}
+        public DbSet<Judoka> Judokas { get; set; }
+    }
         
     }
-}
+
