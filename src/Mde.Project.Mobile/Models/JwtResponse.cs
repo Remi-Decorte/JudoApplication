aa2f057fc436d64f802b168469d51df7ec74bbf1
Remi Decorte <remi.decorte@student.howest.be>
2025-07-25 13:14:06 +0200

    add Authservice and loginmodel for user authentication

---
+++ src/Mde.Project.Mobile/Models/JwtResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mde.Project.Mobile.Models
+{
+    public class JwtResponse
+    {
+        public string Token { get; set; } = string.Empty;
+    }
+}
---
+++ src/Mde.Project.Mobile/Services/AuthService.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Json;
+using Mde.Project.Mobile.Models;
+
+namespace Mde.Project.Mobile.Services;
+
+public class AuthService
+{
+    private readonly HttpClient _httpClient;
+
+    public AuthService()
+    {
+        _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:62160/") 
+        };
+    }
+
+    public async Task<string?> LoginAsync(LoginModel login)
+    {
+        try
+        {
+            var response = await _httpClient.PostAsJsonAsync("auth/login", login);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<JwtResponse>();
+                return result?.Token;
+            }
+
+            return null;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
+
+
