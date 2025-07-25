50f7e7f63eb542138817c93ca818f75101d9ac0a
Remi Decorte <remi.decorte@student.howest.be>
2025-07-25 15:38:44 +0200

    add trainingentrymodel

---
+++ src/Mde.Project.Mobile/Models/TechniqueScoreModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mde.Project.Mobile.Models
+{
+    public class TechniqueScoreModel
+    {
+        public string Technique { get; set; } = string.Empty;
+        public int ScoreCount { get; set; }
+    }
+}
---
+++ src/Mde.Project.Mobile/Models/TrainingEntryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mde.Project.Mobile.Models
+{
+    public class TrainingEntryModel
+    {
+        public DateTime Date { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public List<TechniqueScoreModel> TechniqueScores { get; set; } = new();
+    }
+}
