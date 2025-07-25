76c3a7f065711499dda2a5a4ae761d27c4cc3b4a
Remi Decorte <remi.decorte@student.howest.be>
2025-07-25 15:24:57 +0200

    add trainingentry to store daily training sessions

---
+++ src/Mde.Project.WebApi/Entities/TrainingEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace Mde.Project.WebApi.Entities
+{
+    public class TrainingEntry
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; } = string.Empty; 
+        public DateTime Date { get; set; }
+        public string Type { get; set; } = string.Empty; 
+
+       // public ICollection<TechniqueScore> TechniqueScores { get; set; } = new List<TechniqueScore>();
+    }
+}
