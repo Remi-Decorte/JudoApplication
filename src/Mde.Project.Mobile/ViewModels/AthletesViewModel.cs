8fa0eaf42e53882f396d65d9b04d076f2c647165
remi decorte <remi.decorte@student.howest.be>
2025-08-11 14:29:37 +0200

    Update AthletesViewModel.cs

--- src/Mde.Project.Mobile/ViewModels/AthletesViewModel.cs
+++ src/Mde.Project.Mobile/ViewModels/AthletesViewModel.cs
@@ -1,55 +1,88 @@
-﻿using System.Collections.ObjectModel;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Mde.Project.Mobile.Models;
-using Mde.Project.Mobile.Services;
+using Mde.Project.Mobile.Services.Interfaces;
 
-namespace Mde.Project.Mobile.ViewModels;
-
-public class AthletesViewModel : INotifyPropertyChanged
+namespace Mde.Project.Mobile.ViewModels
 {
-    private readonly JudokaService _judokaService = new();
-
-    private string _selectedCategory = "_60";
-    public string SelectedCategory
+    public class AthletesViewModel : INotifyPropertyChanged
     {
-        get => _selectedCategory;
-        set
+        private readonly IJudokaService _judokaService;
+
+        public AthletesViewModel(IJudokaService judokaService)
         {
-            if (_selectedCategory != value)
+            _judokaService = judokaService;
+            _ = LoadCategoriesAsync();
+        }
+
+        public ObservableCollection<string> Categories { get; } = new();
+        public ObservableCollection<JudokaModel> Judokas { get; } = new();
+
+        private string _selectedCategory = "-60kg";
+        public string SelectedCategory
+        {
+            get => _selectedCategory;
+            set
             {
+                if (_selectedCategory == value) return;
                 _selectedCategory = value;
                 OnPropertyChanged();
-                LoadJudokasAsync();
+                _ = LoadJudokasAsync();
             }
         }
-    }
 
-    public ObservableCollection<string> Categories { get; set; } = new()
-    {
-        "_60", "-66", "-73", "-81", "-90", "-100", "+100"
-    };
-
-    public ObservableCollection<JudokaModel> Judokas { get; set; } = new();
-
-    public AthletesViewModel()
-    {
-        LoadJudokasAsync();
-    }
-
-    private async void LoadJudokasAsync()
-    {
-        var judokas = await _judokaService.GetJudokasByCategoryAsync(SelectedCategory);
-
-        Judokas.Clear();
-        foreach (var j in judokas)
+        private bool _isBusy;
+        public bool IsBusy
         {
-            Judokas.Add(j);
+            get => _isBusy;
+            set { _isBusy = value; OnPropertyChanged(); }
         }
-    }
 
-    public event PropertyChangedEventHandler PropertyChanged;
-    private void OnPropertyChanged([CallerMemberName] string name = null) =>
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        public async Task LoadCategoriesAsync()
+        {
+            Categories.Clear();
+
+            var cats = await _judokaService.GetCategoriesAsync();
+            if (cats?.Count > 0)
+            {
+                foreach (var c in cats) Categories.Add(c);
+                if (!cats.Contains(SelectedCategory))
+                    SelectedCategory = cats.First();
+            }
+            else
+            {
+                // hardcoded
+                var defaults = new[] { "-60kg", "-66kg", "-73kg", "-81kg", "-90kg" };
+                foreach (var c in defaults) Categories.Add(c);
+                if (!defaults.Contains(SelectedCategory))
+                    SelectedCategory = defaults.First();
+            }
+
+            await LoadJudokasAsync();
+        }
+
+        public async Task LoadJudokasAsync()
+        {
+            if (IsBusy || string.IsNullOrWhiteSpace(SelectedCategory)) return;
+            IsBusy = true;
+
+            try
+            {
+                Judokas.Clear();
+                var list = await _judokaService.GetJudokasByCategoryAsync(SelectedCategory);
+                foreach (var j in list) Judokas.Add(j);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 }
+
 
