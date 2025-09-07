1cfc17df6f6429ce7f21a8d05269622441dddcbd
Remi Decorte <remi.decorte@student.howest.be>
2025-09-07 15:28:46 +0200

    Initial Commit

---
+++ src/Mde.Project.Mobile/DTOs/Requests/TechniqueScoreDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Mde.Project.WebApi.DTOs.Requests
+{
+    public class TechniqueScoreDto
+    {
+        [JsonPropertyName("technique")] public string Technique { get; set; } = "";
+        [JsonPropertyName("count")] public int Count { get; set; }
+    }
+}
